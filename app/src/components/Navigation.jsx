785de94212e0d69add6cc6be2532227ee242271a
mujohannes <61717026+mujohannes@users.noreply.github.com>
2024-10-14 07:40:22 +0000

    adding tabs to signup

---
+++ app/src/components/Navigation.jsx
@@ -0,0 +1,7 @@
+import Nav from 'react-bootstrap/Nav'
+export function Navigation() {
+    // returning a navigation bar
+    <Nav>
+        
+    </Nav>
+}
--- app/src/pages/Signup.jsx
+++ app/src/pages/Signup.jsx
@@ -3,27 +3,51 @@ import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Tab from 'react-bootstrap/Tab'
+import Tabs from 'react-bootstrap/Tabs'
 
 export function Signup ( props ) {
     return (
         <>
-            <Container>
+            <Container className="mt-4">
                 <Row>
                     <Col md={{ span: 4, offset: 4 }}>
-                        <Form className="mt-4">
-                            <h2>Sign up for an account</h2>
-                            <Form.Group>
-                                <Form.Label>Email</Form.Label>
-                                <Form.Control type="email" placeholder="you@domain.com"/>
-                            </Form.Group>
-                            <Form.Group>
-                                <Form.Label className="mt-3">Password</Form.Label>
-                                <Form.Control type="password" placeholder="minimum 6 characters" />
-                            </Form.Group>
-                            <Button type="submit" variant="primary" className="my-3 mx-auto d-block w-100">
-                                Sign up
-                            </Button>
-                        </Form>
+                        <Tabs defaultActiveKey="Sign up" fill>
+                            <Tab title="sign up" eventKey="signup">
+                                <Form className="mt-4">
+                                    <h2>Sign up for an account</h2>
+                                    <Form.Group>
+                                        <Form.Label>Email</Form.Label>
+                                        <Form.Control type="email" placeholder="you@domain.com"/>
+                                    </Form.Group>
+                                    <Form.Group>
+                                        <Form.Label className="mt-3">Password</Form.Label>
+                                        <Form.Control type="password" placeholder="minimum 6 characters" />
+                                    </Form.Group>
+                                    <Button type="submit" variant="primary" className="my-3 mx-auto d-block w-100">
+                                        Sign up
+                                    </Button>
+                                    <p>Already have an account? Go to sign in</p>
+                                </Form>
+                            </Tab>
+                            <Tab title="Sign in" eventKey="signin">
+                                <Form className="mt-4">
+                                    <h2>Sign in to your account</h2>
+                                    <Form.Group>
+                                        <Form.Label>Email</Form.Label>
+                                        <Form.Control type="email" placeholder="you@domain.com"/>
+                                    </Form.Group>
+                                    <Form.Group>
+                                        <Form.Label className="mt-3">Password</Form.Label>
+                                        <Form.Control type="password" placeholder="minimum 6 characters" />
+                                    </Form.Group>
+                                    <Button type="submit" variant="primary" className="my-3 mx-auto d-block w-100">
+                                        Sign up
+                                    </Button>
+                                    <p>Don't have an account? Go to sign up</p>
+                                </Form>
+                            </Tab>
+                        </Tabs>
                     </Col>
                 </Row>
             </Container>
