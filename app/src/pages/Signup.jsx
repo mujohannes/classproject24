4c75800c3bbcb65faa4b20ac50a3e709af68bb2c
mujohannes <61717026+mujohannes@users.noreply.github.com>
2024-10-21 10:07:26 +0000

    added password validation

--- app/src/pages/Signup.jsx
+++ app/src/pages/Signup.jsx
@@ -6,9 +6,79 @@ import Col from 'react-bootstrap/Col'
 
 import { createUserWithEmailAndPassword } from 'firebase/auth'
 import { useNavigate } from "react-router-dom"
+import { useState, useEffect } from "react"
 
 export function Signup ( props ) {
+    const [ password, setPassword ] = useState('')
+    const [ validpassword, setValidPassword ] = useState( false )
+
     const navigate = useNavigate()
+
+    const reqNumbers = "0123456789"
+    const reqChars = "abcdefghijklmnopqrstuvwxyz"
+    const reqCaps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const reqSymbols = "!@#?$%^&*()_-+="
+
+    const includesNumbers = () => {
+        const numbersArray = reqNumbers.split('')
+        let result = false
+        numbersArray.forEach( ( number ) => {
+            if( password.includes(number) ) {
+                result = true
+            }
+        })
+        return result
+    }
+
+    const includesChars = () => {
+        const charsArray = reqChars.split('')
+        let result = false
+        charsArray.forEach( (char) => {
+            if( password.includes(char) ) {
+                result = true
+            }
+            
+        } )
+        return result
+    }
+
+    const includesCaps = () => {
+        const capsArray = reqCaps.split('')
+        let result = false
+        capsArray.forEach( (cap) => {
+            if( password.includes(cap) ) {
+                result = true
+            }
+        } )
+        return result
+    }
+
+    const includesSymbols = () => {
+        const symbolsArray = reqSymbols.split('')
+        let result = false
+        symbolsArray.forEach( (symbol) => {
+            if( password.includes(symbol) ) {
+                result = true
+            }
+        })
+        return result
+    }
+
+    useEffect( () => {
+        if( 
+            (password.length >= 8 && password.length <= 15)
+            && includesNumbers() == true
+            && includesChars() == true
+            && includesCaps() == true
+            && includesSymbols() == true
+        ) 
+        {
+            setValidPassword( true )
+        }
+        else {
+            setValidPassword( false )
+        }
+    } , [ password ] )
 
     const signUpUser = ( event ) => {
         event.preventDefault()
@@ -38,11 +108,21 @@ export function Signup ( props ) {
                                 <Form.Label className="mt-3">Password</Form.Label>
                                 <Form.Control 
                                     type="password" 
-                                    placeholder="minimum 6 characters" 
+                                    placeholder="minimum 8 characters" 
                                     name="password"
+                                    value={ password }
+                                    onChange={ (event) => setPassword( event.target.value ) }
                                 />
+                                <Form.Text>
+                                    Password must contain at least one uppercase, lowercase, a number and a symbol and be between 8 and 15 characters long
+                                </Form.Text>
                             </Form.Group>
-                            <Button type="submit" variant="primary" className="my-3 mx-auto d-block w-100">
+                            <Button 
+                            type="submit" 
+                            variant="primary" 
+                            className="my-3 mx-auto d-block w-100"
+                            disabled = { (validpassword) ? false : true }
+                            >
                                 Sign up
                             </Button>
                         </Form>
