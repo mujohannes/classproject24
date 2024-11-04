64844e30ecd8a7fa2b89528a7d648274528162bc
mujohannes <61717026+mujohannes@users.noreply.github.com>
2024-11-04 06:13:02 +0000

    update dark theme switch

--- app/src/components/DarkSwitcher.jsx
+++ app/src/components/DarkSwitcher.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect} from 'react'
 
 export function DarkSwitcher( props ) {
     const [ checked, setChecked ] = useState()
-    let prefLoaded = false
+    //let prefLoaded = false
 
     const htmlElement = document.querySelector('html')
 
@@ -11,27 +11,13 @@ export function DarkSwitcher( props ) {
     useEffect( () => {
         if( checked ) {
             htmlElement.setAttribute('data-bs-theme','dark')
-            storePref('themepref','dark')
+            //storePref('themepref','dark')
         }
         else {
             htmlElement.setAttribute('data-bs-theme','light')
-            storePref('themepref','light')
+            //storePref('themepref','light')
         }
     },[checked])
-
-    useEffect( () => {
-        if( !prefLoaded ) {
-            const pref = readPref('themepref')
-            console.log( pref )
-            if( pref == 'dark' ) {
-                setChecked(true)
-            }
-            else {
-                setChecked(false)
-            }
-            prefLoaded = true
-        }
-    }, [prefLoaded])
 
 
     const storePref = (name,value) => {
